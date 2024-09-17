6a96f17cda1d745e166e9c8a20a3e0938d37f821
Francisco-Gatica06 <148362037+Francisco-Gatica06@users.noreply.github.com>
2024-09-17 18:28:52 -0300

    create map metods

--- src/main/java/org/example/MatrixGame.java
+++ src/main/java/org/example/MatrixGame.java
@@ -2,7 +2,39 @@ package org.example;
 
 public class MatrixGame {
     public static void main(String[] args) {
+        String[][] matrix = new String[10][10];
+        showMap(matrix);
 
     }
 
+    public static void showMap(String[][] matrix) {
+        createOutlineMap(matrix);
+        createPlayZoneMap(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void createOutlineMap (String[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][0] = "#";
+            matrix[i][9] = "#";
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[0][j] = "#";
+                matrix[9][j] = "#";
+            }
+        }
+    }
+
+    public static void createPlayZoneMap (String[][] matrix) {
+        for (int i = 1; i < matrix.length - 1; i++) {
+            for (int j = 1; j < matrix[i].length - 1; j++) {
+                matrix[i][j] = "-";
+            }
+        }
+    }
 }
+
